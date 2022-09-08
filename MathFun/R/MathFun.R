b6878ee1cba67cff3c3bb45084f1e9809861ed80
OHDSI-India <104003093+OHDSI-India@users.noreply.github.com>
2022-09-08 18:49:30 +0530

    Initial Commit

---
+++ MathFun/R/MathFun.R
@@ -0,0 +1,25 @@
+MathFun <- function(num1, num2, opt) {
+  a = num1
+  b = num2
+  if(opt == 1) {
+    c = a + b
+  }
+  else if (opt == 2){
+    c = a - b
+  }
+  else if (opt == 3){
+    c = a * b
+  }
+  else if (opt == 4){
+    c = a / b
+  }
+  else if (opt == 5){
+    c = a ^ b
+  }
+  else {
+    c = "Enter a correct operation"
+  }
+  return(c)
+}
+
+
